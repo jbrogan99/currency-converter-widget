886c948392bbf0dedd5422bbed7081accfb47328
jbrogan99 <jack.brogan99@gmail.com>
2023-03-03 11:32:09 +0000

    Dynamic drop downs and default value/currency

--- currency.js
+++ currency.js
@@ -15,3 +15,35 @@ async function fetchText() {
   console.log("return", names_values);
   return names_values;
 }
+
+let currencyForm1 = document.getElementById("firstInputCurrency");
+let dropDownOne = document.getElementById("currencyFrom");
+let dropDownTwo = document.getElementById("currencyTo");
+let inputValue1 = document.getElementById("value1");
+
+fetchText().then((names_values) => {
+  // get names and values after they have fully loaded
+  const dropDownHolderFrom = document.getElementById("currencyFrom");
+  const dropDownHolderTo = document.getElementById("currencyTo");
+  names_values.forEach((el) => {
+    // loop round names & values
+    const dropDownOptions = document.createElement("option");
+    const dropDownOptions2 = document.createElement("option");
+    dropDownHolderFrom.append(dropDownOptions);
+    dropDownOptions.append(el[0]); // assign names to drop down
+    dropDownOptions.setAttribute("value", el[1]); // assign exchange rates to corresponding dropdown name
+    if (el[0] == "GBP") {
+      //set GBP as the initial drop down value
+      inputValue1.defaultValue = Number(el[1]).toFixed(2);
+      dropDownOptions.setAttribute("selected", "selected");
+    }
+
+    dropDownHolderTo.append(dropDownOptions2);
+    dropDownOptions2.append(el[0]);
+    dropDownOptions2.setAttribute("value", el[1]);
+    if (el[0] == "USD") {
+      //set USD as the initial drop down value
+      dropDownOptions2.setAttribute("selected", "selected");
+    }
+  });
+});
